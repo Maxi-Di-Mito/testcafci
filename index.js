8156d3376ba43bee5a4f7a731399e9537bce1215
alumno <alumno>
2017-11-16 22:07:16 -0300

    Fetch fondos

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetch = require('fetch');
 const fs = require('fs');
-
+/*
 fetch.fetchUrl('https://api.cafci.org.ar/fondo/456/clase/873/ficha',{
     method: 'GET',
     rejectUnauthorized : false
@@ -9,7 +9,27 @@ fetch.fetchUrl('https://api.cafci.org.ar/fondo/456/clase/873/ficha',{
     const parsed = JSON.parse(data);
     const d = parsed.data.info.diaria.actual;
     console.info('data', JSON.stringify(d));
+});*/
+
+
+
+fetch.fetchUrl('https://api.cafci.org.ar/fondo?estado=1&include=entidad;depositaria,entidad;gerente,tipoRenta,region,benchmark,horizonte,duration,tipo_fondo,clase_fondo&limit=0&order=clase_fondos.nombre',{
+    method: 'GET',
+    rejectUnauthorized : false
+}, (err, meta, data) => {
+    err && console.error(err);
+    const parsed = JSON.parse(data);
+    const procesado = parsed.data.map( (item) => {
+        return {
+            id: item.id,
+            name: item.nombre,
+            clase: item.clase_fondos[0]
+        }
+    });
+    console.info('LARGO: ',JSON.stringify(procesado));
 });
+
+
 
 
 
